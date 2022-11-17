2a08b8e95ec072669fa7e535d992f139fd9459fc
Sansui233 <sansuilnm@gmail.com>
2022-11-17 17:43:08 +0800

    update dep to fix pnpm + ts module bug

--- pages/memos.tsx
+++ pages/memos.tsx
@@ -35,7 +35,7 @@ export default function Memos({ memoposts, pagelimit }: Props) {
     let page = 0
     if (typeof (router.query.p) === 'string') {
       page = parseInt(router.query.p)
-      if (page === NaN) {
+      if (isNaN(page)) {
         console.error('Wrong query p=', router.query.p)
         return
       }
@@ -64,7 +64,7 @@ export default function Memos({ memoposts, pagelimit }: Props) {
   const currPage = (() => {
     if (typeof (router.query.p) === 'string') {
       const page = parseInt(router.query.p)
-      if (page !== NaN) {
+      if (isNaN(page)) {
         return page
       }
     }
